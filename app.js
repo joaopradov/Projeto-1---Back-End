09df53a47bc472ab700a05d1f9c52ea2426bca92
João Vitor Prado <joaopradovitor99@gmail.com>
2025-09-21 13:48:07 -0300

    :bug: fix App.js

--- app.js
+++ app.js
@@ -7,20 +7,20 @@ const app = express();
 app.engine('hbs', engine({
     extname: '.hbs',
     defaultLayout: 'main',
-    layoutsDir: path.join(__dirname, 'src/views/layouts'),
-    partialsDir: path.join(__dirname, 'src/views/partials')
+    layoutsDir: path.join(__dirname, 'views/layouts'),
+    partialsDir: path.join(__dirname, 'views/partials')
 }));
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'src/views'));
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'src/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('/public/home', { 
+    res.render('public/home', { 
         pageTitle: 'Portfólio de Engenharia de Software'
     });
 });

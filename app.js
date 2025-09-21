7c7f71e552e0ab03c37cd6deed165d2fe2241858
João Vitor Prado <joaopradovitor99@gmail.com>
2025-09-21 14:26:08 -0300

    :lipstick: Estilização Principal

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const app = express();
 app.engine('hbs', engine({
     extname: '.hbs',
     defaultLayout: 'main',
-    layoutsDir: path.join(__dirname, 'views/layouts'),
-    partialsDir: path.join(__dirname, 'views/partials')
+    layoutsDir: path.join(__dirname, 'src/views/layouts'),
+    partialsDir: path.join(__dirname, 'src/views/partials')
 }));
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'src/views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src/public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

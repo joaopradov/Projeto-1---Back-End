52cb8a899715064bc7ea0deeca29dc3aa33bd7fd
João Vitor Prado <joaopradovitor99@gmail.com>
2025-09-21 13:22:58 -0300

    :construction: App.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+
+app.engine('hbs', engine({
+    extname: '.hbs',
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'src/views/layouts'),
+    partialsDir: path.join(__dirname, 'src/views/partials')
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'src/views'));
+
+app.use(express.static(path.join(__dirname, 'src/public')));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        pageTitle: 'Portfólio de Engenharia de Software'
+    });
+});
+
+module.exports = app;
